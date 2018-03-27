638ea2edf6c94f20ed567f65b396be8caf3e08aa
Lucas Carey <lucas.carey@upf.edu>
2018-03-27 16:59:20 +0200

    a

--- Figures/Lucas__EvolConf/fig.m
+++ Figures/Lucas__EvolConf/fig.m
@@ -1,26 +1,24 @@
 %% barplots showing R^2 for model to predict evolution data from distance-to-end or cdc20 arrest
 % AM March 2018
 
-%% load data
+%% set paths
 cd('~/Develop/Mendoza__ReplicationEvolution/Figures/Lucas__EvolConf');
 addpath(genpath('~/Develop/matlab'));
+
+%% for SNP & InDel frequency 
 load('~/Develop/Mendoza__ReplicationEvolution/Data/DS_stat__200bp.mat');
 
-%% for SNP/InDel frequency 
 DS.dist_log = log2(DS.dist_to_the_end_kb);
 DS = DS(: , {'dist_log' , 'Trep_spline' , 'percent_unreplicated_not_trimmed_cdc20_smooth' , 'freq_SNP' , 'freq_indel'});
 idx = find(~isnan(DS.percent_unreplicated_not_trimmed_cdc20_smooth));
-T = DS(idx , :);
-rand_vec = rand(length(T) , 1);
-T.random_num = NaN(length(T) , 1);
-for I = 1:length(T)
-    T.random_num(I) = rand_vec(I);
-end
-T = sortrows(T , 'random_num');
+T = dataset2table( DS(idx , :) ) ;
+
+% shuffle dataset
+T = T( randperm(nrows(T)) ,:);
 
 K = 25;
-N = round(length(T)/K);
-all_idx = [1:length(T)];
+N = round(size(T,1)/K);
+all_idx = [1:size(T,1)];
 idx = cell(K , 1);
 for I = 1:K-1
     temp_idx = all_idx(1:N);
@@ -29,6 +27,7 @@ for I = 1:K-1
 end 
 idx{K} = all_idx;
 
+%% fit & plot data
 idx_output = [4 5];
 title_names = {'SNP frequency' , 'InDel frequency'};
 save_names = {'SNP__R2' , 'Indel__R2'};
@@ -38,13 +37,13 @@ for Z = 1:2
     R_train = cell(length(parameters_idx),1);
     R_test = cell(length(parameters_idx),1);
     for I = 1:length(parameters_idx)
-        X = double( T(:,parameters_idx{I}));
-        Y = double( T(:, idx_output(Z))) ;
+        X = table2array( T(:,parameters_idx{I}));
+        Y = table2array( T(:, idx_output(Z))) ;
         temp_train = NaN(K,1);
         temp_test = NaN(K,1);
         for J = 1:K
             
-            idx_train = setdiff([1:length(T)] , idx{J});
+            idx_train = setdiff([1:size(T,1)] , idx{J});
             idx_test = idx{J};
             Mdl = fitglm(X(idx_train , :) , Y(idx_train , :) );
             Y_train = predict(Mdl , X(idx_train , :));
@@ -64,27 +63,45 @@ for Z = 1:2
         R_test{I} = temp_test;
     end
 
+    % plot all four bars
     figure; hold on; grid on; set(gca , 'FontSize' , 8); 
     data = [R_test{1} R_test{2} R_test{3} R_test{4} ];
     mean_data = mean(data);
     std_data = std(data);
-	clrs = parula(16);
-    for I = 1:4
-        bar(I , mean_data(I) , 'FaceColor' , clrs(4*I-2,:));
-        errorbar(I , mean_data(I) , std_data(I) , 'LineWidth' , 2, 'color' , [.2 .2 .2]);
+	clrs = get(gca,'ColorOrder');
+    clrs(3,:) = clrs(4,:) ;
+    clrs(4,:) = [0 0 0] ;
+    for I = 1:numel(mean_data)
+        bar(I , mean_data(I) , 'FaceColor' , clrs(I,:));
+        errorbar(I , mean_data(I) , std_data(I) , 'LineWidth' , 1, 'color' , [.5 .5 .5]);
     end
     [~,p] = ttest2(data(:,3) , data(:,4));
-    if p < 0.0001
-        text(3 , nanmax(nanmax(data)) , 'p < 0.0001' , 'FontSize' , 6);
-    else
-        text(3 , nanmax(nanmax(data)) , sprintf('p = %.3f' , p) , 'FontSize' , 6);
-    end
     ylabel('R^{2}');
-    set(gcf , 'PaperPosition' , [0 0 4 4]);
-    title(title_names{Z}); 
+    set(gcf , 'PaperPosition' , [0 0 3 6]);
+    title( sprintf( '%s 3v4 p=%0.06f' , title_names{Z} , p )); 
     xlim([.5 4.5]);
-    set(gca , 'Xtick' , [1:4]);
+    set(gca , 'Xtick' , [ ]);
     print('-dpng' , save_names{Z} , '-r600');
+    close ;
+    
+    % plot only two bars
+    figure; hold on; grid on; set(gca , 'FontSize' , 8); 
+    data = [R_test{3} R_test{4} ];
+    mean_data = mean(data);
+    std_data = std(data);
+	clrs = get(gca,'ColorOrder');
+    for I = 1:numel(mean_data)
+        bar(I , mean_data(I) , 'FaceColor' , clrs(I,:));
+        errorbar(I , mean_data(I) , std_data(I) , 'LineWidth' , 1, 'color' , [.5 .5 .5]);
+    end
+    [~,p] = ttest2(data(:,1) , data(:,2));
+    ylabel('R^{2}');
+    set(gcf , 'PaperPosition' , [0 0 2 6]);
+    title( sprintf( '%s 3v4 p=%0.06f' , title_names{Z} , p )); 
+    xlim([.5 2.5]);
+    set(gca , 'Xtick' , [ ]);
+    print('-dpng' , [ save_names{Z} '_2' ] , '-r600');
+    close ;    
 end
 
 

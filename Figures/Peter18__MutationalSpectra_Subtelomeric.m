0362e76f691c71180066d1fc3be55c6c51f8e4ce
Lucas Carey <lucas.carey@upf.edu>
2018-07-17 14:53:35 +0200

    a

--- Figures/Peter18__MutationalSpectra_Subtelomeric.m
+++ Figures/Peter18__MutationalSpectra_Subtelomeric.m
@@ -1,41 +1,61 @@
-T = readtable('x2.tab','FileType','text');
-T.Subtel50 = zeros(height(T),1);
-T.Subtel25 = zeros(height(T),1);
-T.Subtel50( T.Var2 < 50000) = 1 ; 
-T.Subtel25( T.Var2 < 25000) = 1 ; 
-T.Subtel50( T.Var2 < (max(T.Var2)-50000)) = 1 ; 
-T.Subtel25( T.Var2 < (max(T.Var2)-25000)) = 1 ; 
-T.Alt1 = regexprep(T.Var4,',.*','');
-%%
-AllMuts=grpstats(T,{'Subtel25'},'sum','DataVars','Var1');
-G=grpstats(T,{'Var3' 'Alt1' 'Subtel25'},'sum','DataVars','Var1');
-G = G( cellfun(@length,G.Alt1)==1 , :);
-G = G( cellfun(@length,G.Var3)==1 , :);
-%%
-all_subel__nonsubtel = [sum(G.GroupCount(G.Subtel25==1)) sum(G.GroupCount(G.Subtel25==0))];
-figure;
-hold on ;
+%% load data
+DATADIR = '~/Data/Peter18/' ;
+T = readtable([ DATADIR 'nocomma.tab' ] ,'FileType','text','Delimiter','\t','Format','%d%d%s%s');
+T.Properties.VariableNames = {'chr_num' 'pos' 'ref' 'alt'};
+%% Define Subtelomeric regions
+SUBTEL = 25*1000 ; 
+T.Subtel = zeros(height(T),1);
+T.Subtel( T.pos < SUBTEL) = 1 ; 
+T.Subtel( T.pos > (max(T.pos)-SUBTEL)) = 1 ; 
+
+%% keep only SNPs
+T = T( cellfun(@length,T.alt)==1 , :);
+T = T( cellfun(@length,T.ref)==1 , :);
+
+
+%% summary stats
+G=grpstats(T,{'chr_num' 'ref' 'alt' 'Subtel'}, 'sum','DataVars','chr_num');
+G.sum_chr_num = []; 
+
+
+%% compute stats for each mutation & chr
+G.FE_p = NaN(height(G),1);
+G.FE_or = NaN(height(G),1);
 for I = 1:2:height(G)
-    non_subtel__count =  G.GroupCount(I) ;
-    subtel__count =  G.GroupCount(I+1) ;
-    txt = sprintf( '%s->%s' , G.Var3{I}  , G.Alt1{I} );
-    [~,p,or] = fishertest( [ all_subel__nonsubtel ; subtel__count non_subtel__count]) ;
-    plot( non_subtel__count , subtel__count , '.k');
-    if p < 0.05
-        text( non_subtel__count , subtel__count , txt);
-    end
-end
-xlabel('non-subtel count ')
-ylabel('subtel count')
-%%
-lr = log2(all_subel__nonsubtel(1)/all_subel__nonsubtel(2)); 
-for I = 1:2:height(G)
-    non_subtel__count =  G.GroupCount(I) ;
-    subtel__count =  G.GroupCount(I+1) ;
-    [~,p,or] = fishertest( [ all_subel__nonsubtel ; subtel__count non_subtel__count]) ;
-    txt = sprintf( '%s->%s' , G.Var3{I}  , G.Alt1{I} );
-    fprintf( '%s\t%0.02f\t%0.03f\n' , txt, log2(subtel__count./non_subtel__count) - lr  , p );
+    st = sum(G.GroupCount( G.Subtel==1 & G.chr_num==G.chr_num(I)));
+    nonst = sum(G.GroupCount( G.Subtel==0 & G.chr_num==G.chr_num(I)));
+    [~,p,or] = fishertest( [ nonst st ; G.GroupCount(I) G.GroupCount(I+1)] );
+   G.FE_p(I) = log10(p);
+   G.FE_or(I) = or.OddsRatio ;
 end
 
-%%
-T = readtable('~/Downloads/nocomma.tab','FileType','text);
+
+G2 = grpstats( G( ~isnan(G.FE_p) ,:) , {'ref' 'alt'},{'mean' 'std'},'DataVars',{'FE_p' 'FE_or'})
+
+% %     %%
+% % all_subel__nonsubtel = [sum(G.GroupCount(G.Subtel25==1)) sum(G.GroupCount(G.Subtel25==0))];
+% % figure;
+% % hold on ;
+% % for I = 1:2:height(G)
+% %     non_subtel__count =  G.GroupCount(I) ;
+% %     subtel__count =  G.GroupCount(I+1) ;
+% %     txt = sprintf( '%s->%s' , G.Var3{I}  , G.Alt1{I} );
+% %     [~,p,or] = fishertest( [ all_subel__nonsubtel ; subtel__count non_subtel__count]) ;
+% %     plot( non_subtel__count , subtel__count , '.k');
+% %     if p < 0.05
+% %         text( non_subtel__count , subtel__count , txt);
+% %     end
+% % end
+% % xlabel('non-subtel count ')
+% % ylabel('subtel count')
+% % %%
+% % lr = log2(all_subel__nonsubtel(1)/all_subel__nonsubtel(2)); 
+% % for I = 1:2:height(G)
+% %     non_subtel__count =  G.GroupCount(I) ;
+% %     subtel__count =  G.GroupCount(I+1) ;
+% %     [~,p,or] = fishertest( [ all_subel__nonsubtel ; subtel__count non_subtel__count]) ;
+% %     txt = sprintf( '%s->%s' , G.Var3{I}  , G.Alt1{I} );
+% %     fprintf( '%s\t%0.02f\t%0.03f\n' , txt, log2(subtel__count./non_subtel__count) - lr  , p );
+% % end
+% % 
+% % %%

ad23c68a1e81cf3e78353de233be0325e16cfae6
Lucas Carey <lucas.carey@upf.edu>
2018-08-17 18:13:28 +0200

    a

--- Figures/G4_VaryThreshold_ShowAlongAllChromosomes.m
+++ Figures/G4_VaryThreshold_ShowAlongAllChromosomes.m
@@ -25,30 +25,45 @@ DS.smooth_residual_1  = 100*DS.percent_underreplicated_cdc20 - DS.underrep_smoot
 DS.smooth_residual_2  = 100*DS.percent_underreplicated_cdc20 - DS.underrep_smoothed_2 ;
 
 
+%% join two datasets
+
+DS.roundpos = round(DS.middle_point_kb);
+DS_FEATURES.roundpos = round(DS_FEATURES.middle_point_kb);
+DS2 = join( DS , DS_FEATURES(:,{'chr_num' 'roundpos' 'TYPE'}) , 'Key',{'chr_num' 'roundpos'} ,'type','inner' ,'MergeKeys',true);
+G2  = grpstats( DS2 , {'chr_num' 'roundpos' 'TYPE'} , 'mean' ,'DataVars'...
+    , {'percent_underreplicated_cdc20' 'smooth_residual_1' 'smooth_residual_2'} );
+Ys2 = DS2.smooth_residual_1 ; 
+Ys = DS.smooth_residual_1 ; 
+
+
 %% plot an example of the smoothed data
-chrI=4 ; 
+chrI=10 ; 
 THRESH = [99.5];
 GREEN = [0.1818    0.5909    0.4000] ; 
 idx = DS.chr_num == chrI ; 
+Y = 100*DS.percent_underreplicated_cdc20 ;
+Y2 = 100*G2.mean_percent_underreplicated_cdc20 ;
+
 fh = figure('units','centimeters','position',[5 5 15 10]); hold on ;
-plot( DS.start_point(idx)./1000 , 100*DS.percent_underreplicated_cdc20(idx) ,'-','Color',GREEN)
+plot( DS.start_point(idx)./1000 , Y(idx) ,'-','Color',GREEN)
 plot( DS.start_point(idx)./1000 , DS.underrep_smoothed_1(idx) ,'-r','LineWidth',1)
-plot( DS.start_point(idx)./1000 , DS.underrep_smoothed_2(idx) ,'-c','LineWidth',1)
-
-%plot( DS.start_point(idx)./1000 , DS.smooth_residual(idx) ,'-c')
-
+%plot( DS.start_point(idx)./1000 , DS.underrep_smoothed_2(idx) ,'-c','LineWidth',1)
 xlabel('Chromsome position (kb)')
 ylabel('% under-replicated')
 axis tight; 
 
+
 %G quads
 idx_g4_2 = DS.G4 > prctile(DS.G4,THRESH(1))  ; 
-plot(DS.start_point(idx & idx_g4_2 )./1000  , 100*DS.percent_underreplicated_cdc20(idx & idx_g4_2) ,'sk','MarkerFaceColor','k')
+plot(DS.start_point(idx & idx_g4_2 )./1000  , Y(idx & idx_g4_2) ,'sk','MarkerFaceColor','k')
 
 % transposons
-idx = regexpcmp(DS_FEATURES.TYPE,'transpo') & DS_FEATURES.chr_num == chrI ;
-if sum(idx)>0 , plot(DS_FEATURES.middle_point_kb(idx) , -8 , 'sm') , end
+idx = regexpcmp(G2.TYPE,'transposable_element_gene') & G2.chr_num == chrI ;
+if sum(idx)>0 , plot(G2.roundpos(idx) , Y2(idx) , 'sm') , end
 
+% fragile sites
+idx = regexpcmp(G2.TYPE,'terminal_del_dup') & G2.chr_num == chrI ;
+if sum(idx)>0 , plot(G2.roundpos(idx) , Y2(idx)  , 'sc','MarkerFaceColor','c') , end
 
 title(['chr ' num2roman(chrI)]);
 
@@ -56,7 +71,6 @@ ylim([-9 40])
 
 %% boxplots showing G-quads & transposon higher %underrep even after smoothing
 %% G4 enrichment by random sampling
-Y = DS.smooth_residual_1 ; 
 
 THRESH = 99.9 ;
 G4idx = DS.G4 > prctile(DS.G4,THRESH);
@@ -65,42 +79,64 @@ M = NaN(0);
 N = 2e3 ; 
 for I = 1:N
     idx = randsample( find(~G4idx) , 100 );
-    M(I,1) = mean( Y(idx));
+    M(I,1) = median( Ys(idx));
 end
 for I = 1:N
     idx = randsample( find(G4idx) , 100 , true );
-    M(I,2) = mean( Y(idx));
+    M(I,2) = median( Ys(idx));
 end
-fh = figure('units','centimeters','position',[5 5 5 8]);hold on ; 
+fh = figure('units','centimeters','position',[5 5 5 8]);  
 bh = boxplot(M,'Notch','on','Symbol','') ; 
 ylim([-2 prctile(M(:,2),99)])
 set(gca,'xticklabel',{'random' 'G-quads'}) ; 
 ylabel('Residuals (%underrep - smoothed)'); 
 set(bh(5,2),'Color','k','LineWidth',2)
 set(bh(5,1),'Color',GREEN,'LineWidth',2)
-% Transposon enrichment
-roundpos = round(DS.middle_point_kb);
-TRANSidx = ismember( roundpos , round(DS_FEATURES.middle_point_kb( regexpcmp(DS_FEATURES.TYPE,'transpo'))));
+%% Transposon enrichment
+
+
+TRANSidx =  regexpcmp(DS2.TYPE,'transposable_element_gene');
 
 fprintf('%0.02f %d transposons\n' , mean(TRANSidx) , sum(TRANSidx));
 M = NaN(0);
-N = 2e3 ; 
+N = 1e4 ; 
 for I = 1:N
     idx = randsample( find(~TRANSidx) , 100 );
-    M(I,1) = mean( Y(idx));
+    M(I,1) = median( Ys2(idx));
 end
 for I = 1:N
     idx = randsample( find(TRANSidx) , 100 , true );
-    M(I,2) = mean( Y(idx));
+    M(I,2) = median( Ys2(idx));
 end
-fh = figure('units','centimeters','position',[5 5 5 8]);hold on ; 
+fh = figure('units','centimeters','position',[5 5 5 8]) ; 
 bh = boxplot(M,'Notch','on','Symbol','') ; 
 ylim([-2 prctile(M(:,2),99)])
-set(gca,'xticklabel',{'random' 'tranposons'}) ; 
+set(gca,'xticklabel',{'random' 'transposons'}) ; 
 ylabel('Residuals (%underrep - smoothed)'); 
 set(bh(5,2),'Color','m','LineWidth',2)
 set(bh(5,1),'Color',GREEN,'LineWidth',2)
 
+%% Fragile sites
+FRAGidx = regexpcmp(G2.TYPE,'del_dup');
+
+fprintf('%0.02f %d del-dup\n' , mean(FRAGidx) , sum(FRAGidx));
+M = NaN(0);
+N = 1e4 ; 
+for I = 1:N
+    idx = randsample( find(~FRAGidx) , 100 );
+    M(I,1) = median( Ys2(idx));
+end
+for I = 1:N
+    idx = randsample( find(FRAGidx) , 100 , true );
+    M(I,2) = median( Ys2(idx));
+end
+fh = figure('units','centimeters','position',[5 5 5 8]) ; 
+bh = boxplot(M,'Notch','on','Symbol','') ; 
+%ylim([-2 prctile(M(:,2),99)])
+set(gca,'xticklabel',{'random' 'fragile sites'}) ; 
+ylabel('Residuals (%underrep - smoothed)'); 
+set(bh(5,2),'Color','c','LineWidth',2)
+set(bh(5,1),'Color',GREEN,'LineWidth',2)
 
 
 

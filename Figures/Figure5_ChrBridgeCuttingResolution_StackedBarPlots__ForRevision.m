7939a27dd58bd8df275cd8d205070e6df5ec55be
Lucas Carey <lucas.carey@gmail.com>
2019-10-30 10:57:11 +0800

    a

--- Figures/Figure5_ChrBridgeCuttingResolution_StackedBarPlots__ForRevision.m
+++ Figures/Figure5_ChrBridgeCuttingResolution_StackedBarPlots__ForRevision.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clrs = cbrewer('qual','Dark2',8) ; 
 FA = 0.75 ; 
 %% cdc15-as vs top2-ts cdc15-as
@@ -34,7 +33,6 @@ bar(2 , B_resolve(2) , 'FaceColor' ,clrs(7,:) ,'FaceAlpha',FA);
 errorbar( 1 , mean(m1) , std(m1) , '.' ,'LineWidth',2 ,'Color',clrs(1,:)); 
 errorbar( 2 , mean(m2) , std(m2) , '.' ,'LineWidth',2 ,'Color',clrs(6,:)); 
 
-=======
 
 %% test
 clrs = cbrewer('qual','Dark2',8) ; 
@@ -56,12 +54,10 @@ hold on ;
 bar( [ 100 100] , 'FaceColor' , [.95 .95 .95]);
 bar(1 , B_resolve(1) , 'FaceColor' ,clrs(3,:) ,'FaceAlpha',0.5);
 bar(2 , B_resolve(2) , 'FaceColor' ,clrs(1,:) ,'FaceAlpha',0.5);
->>>>>>> 8559f28f298494896a81778cdcc4d988f737d0c3
 set(gca,'xtick',[])
 xlim([0.5 2.5])
 ylabel('% of cells with a visible bridge')
 
-<<<<<<< HEAD
 x = [ sum(V_B_resolve{1}==0)  sum(V_B_resolve{1}==1) ; sum(V_B_resolve{2}==0)  sum(V_B_resolve{2}==1) ] ; 
 [~,p,~] = fishertest(x)
 
@@ -137,7 +133,6 @@ ylabel('% of anaphase cells with RPA foci')
 
 x = [ sum(V_RFA{1}==0)  sum(V_RFA{1}==1) ; sum(V_RFA{2}==0)  sum(V_RFA{2}==1) ] ; 
 [~,p,~] = fishertest(x)
-=======
 %%
 
 A_resolved = [10.309 12.680  ] ; 
@@ -165,4 +160,3 @@ bar( 5 , 100-C_cut(5) , 'FaceColor', clrs(5,:),  'FaceAlpha',0.5);
 ylabel('% of cells with bridges')
 set(gca,'xtick',[])
 xlim([0.5 5.5])
->>>>>>> 8559f28f298494896a81778cdcc4d988f737d0c3
